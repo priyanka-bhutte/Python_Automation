15d5ec9be2ebb9c41af4e876ab3d6a04f627789d
priyanka-bhutte <132783153+priyanka-bhutte@users.noreply.github.com>
2024-06-02 15:10:38 +0530

    Add files via upload

---
+++ DirectoryAutomation10.py
@@ -0,0 +1,76 @@
+import sys
+import os
+import time
+
+def DirectoryWatcher(DirName, FileName):
+    Count = 0
+
+    flag = os.path.isabs(DirName)
+
+    if (flag == False):
+        DirName = os.path.abspath(DirName)
+        
+    exist = os.path.isdir(DirName)
+
+    if(exist == True):
+        for foldername, subfoldername, filename in os.walk(DirName):
+            for name in filename:
+                if name == FileName:
+                    print("File is present in the directory")
+                    break
+                    
+    else:
+        print("There is no such directory")
+        
+def main():
+    print("---------------- Directory Watcher -------------------")
+
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("This script is used to perform Directory traversal")
+            exit()
+
+        if(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("Usage of the script : ")
+            print("Name_Of_File  Name_Of_Directory  Name_Of_File")
+            exit()
+
+    if(len(sys.argv) == 3):
+        try:
+            starttime = time.time()
+            DirectoryWatcher(sys.argv[1],sys.argv[2])
+            endtime = time.time()
+
+            print("Time required to execute the script is : ",endtime-starttime)
+
+        except Exception as obj2:
+            print("Unable to perform the task due to ", obj2)
+            
+    else:
+        print("Invalid option")
+        print("Use --h option to get the help and use --u option to get the usage of application")
+        exit()
+    
+    print("--------- Thank you for using our script -------------")
+    print("------------- Marvellous Infosystems -----------------")
+
+if __name__ == "__main__":
+    main()
+
+# python DirectoryAutomation.py Study
+
+# sys.argv[0]   DirectoryAutomation.py      
+# sys.argv[1]   Study
+
+# len(sys.argv)     2  
+# Output:
+# C:\Users\Priyanka\Desktop\Python_2024\26-05-2024\drive-download-20240526T181700Z-001>python DirectoryAutomation10.py Study ReadFile.p
+# y
+# ---------------- Directory Watcher -------------------
+# File is present in the directory
+# File is present in the directory
+# Time required to execute the script is :  0.0
+# --------- Thank you for using our script -------------
+# ------------- Marvellous Infosystems -----------------
+
+# C:\Users\Priyanka\Desktop\Python_2024\26-05-2024\drive-download-20240526T181700Z-001>  
